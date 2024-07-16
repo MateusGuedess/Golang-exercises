5dd37910150562d240a1b5c6c644d0e15c628fc2
Mateus Guedes <mxteusgued@gmail.com>
2024-07-16 00:23:08 -0300

    create mapas e estatisticas

--- exercise_three.go
+++ exercise_three.go
@@ -1,6 +1,10 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	palavras := os.Args[1:]
@@ -15,6 +19,23 @@ func colherEstatisticas(palavras[]string) (map[string]int) {
 	estatisticas := make(map[string]int)
 
 	for _, palavra := range palavras {
-		
+		inicial := strings.ToUpper(palavra[:1])
+		contador, encontrado := estatisticas[inicial]
+
+		if encontrado {
+			estatisticas[inicial] = 1 + contador
+		} else {
+			estatisticas[inicial] = 1
+		}
+	}
+
+	return estatisticas
+}
+
+func imprimir(estatisticas map[string]int) {
+	fmt.Println("Contagem de palavras iniciadas em cada letra: ")
+
+	for inicial, contador := range estatisticas {
+		fmt.Printf("%s = %d\n", inicial, contador)
 	}
 }
