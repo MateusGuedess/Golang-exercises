bfe44a4dfc2e7504fec752b4be3d8d73aa90300b
Mateus Guedes <mxteusgued@gmail.com>
2024-07-20 00:15:05 -0300

    create HOF

--- cap6-closures/fibonacci.go
+++ cap6-closures/fibonacci.go
@@ -3,15 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	a, b := 0, 1
 
-	fib	 := func() int {
-		a, b = b, a + b
 
-		return a
-	}
+	gerarFib := GerarFibonacci(20)
 
-	for i := 0; i < 8; i++ {
-		fmt.Printf("%d ", fib())
+	gerarFib()
+
+}
+
+func GerarFibonacci(qtGerar int) (func()) {
+	return func() {
+		a, b := 0, 1;
+
+		fib := func() int {
+			a, b = b, a + b
+	
+			return a
+		}
+
+
+		for i := 0; i < qtGerar; i++ {
+			fmt.Printf("%d ", fib())
+		}
 	}
 }
